994644839419c58bfc94829f2ab85c78b4cfd0e5
Alla Magina <babara@flylady.su>
2024-12-30 17:26:53 +0300

    Добавлено логирование и отправка статистики

---
+++ src/utils/history.ts
@@ -0,0 +1,68 @@
+// src/utils/history.ts
+import * as fs from 'fs';
+import * as path from 'path';
+import { Context, Telegraf } from 'telegraf';
+import { Update } from 'telegraf/typings/core/types/typegram';
+
+export interface UserAction {
+  time: string;
+  action: string;
+}
+
+export let userNavigationHistory: { [userId: string]: UserAction[] } = {};
+
+// Функция для добавления состояния в историю
+export const addToHistory = (userId: string, action: string) => {
+  const timestamp = new Date().toISOString();
+  if (!userNavigationHistory[userId]) {
+    userNavigationHistory[userId] = [];
+  }
+  userNavigationHistory[userId].push({ time: timestamp, action });
+  console.log('userNavigationHistory: ', userNavigationHistory);
+};
+
+// Функция для записи истории в файл
+const writeHistoryToFile = async (bot: Telegraf<Context<Update>>) => {
+  const filePath = path.join(__dirname, 'user_history.txt');
+  let fileContent = '📊 Статистика взаимодействия:\n\n';
+
+  for (const userId in userNavigationHistory) {
+    const userActions = userNavigationHistory[userId];
+    const chat = await bot.telegram.getChat(userId);
+
+    if (chat && chat.type === 'private') {
+      const userName = chat.first_name || 'Не указано';
+      const userUsername = chat.username ? `@${chat.username}` : 'Не указано';
+
+      fileContent += `👤 Пользователь: ${userName}\n`;
+      fileContent += `Username: ${userUsername}\n`;
+      fileContent += `ID: ${userId}\n`;
+      fileContent += `История взаимодействия:\n`;
+
+      userActions.forEach(({ time, action }) => {
+        fileContent += `🕒 ${time} - ${action}\n`;
+      });
+
+      fileContent += '\n';
+    }
+  }
+
+  // Записываем содержимое в файл
+  fs.writeFileSync(filePath, fileContent);
+  return filePath;
+};
+
+export const sendStatisticsToAdmin = async (
+  bot: Telegraf<Context<Update>>,
+  tgId: string,
+) => {
+  try {
+    const filePath = await writeHistoryToFile(bot);
+    await bot.telegram.sendDocument(tgId, { source: filePath });
+    // Очистка истории после успешной отправки
+    userNavigationHistory = {};
+    console.log('История взаимодействий очищена.');
+  } catch (error) {
+    console.error('Ошибка при отправке статистики администратору:', error);
+  }
+};
